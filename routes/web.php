e74266247c9529895e6d2395c043b36472239e07
fran gregori <frangregori61@gmail.com>
2025-04-14 11:25:30 +0200

    creada ruta home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', WelcomeController::class);
+Route::get('/home', HomeController::class);
